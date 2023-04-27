dc43c9f198904d0e51e7c735913e9e581094df22
inoricode28 <inori237@gmail.com>
2023-04-26 21:24:20 -0500

    Sexto Viaje

--- CursoJava001/src/App.java
+++ CursoJava001/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nom;
+        // Crear el Obejto Scanner luego de importar y le asignamos el nombre que deseamos
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("-------Entrada de Datos ------");
+        System.out.println("Ingrese su nombre");
+        
+        nom=entrada.nextLine();
+        System.out.println("Su nombre es: "+nom);
+        
+        entrada.close();
     }
 }
